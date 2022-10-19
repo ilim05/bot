dbcb5aaf36f967d4eca11d15153465bfa5e0af78
ilim05 <ilimzutanov4@gmail.com>
2022-10-19 19:29:35 +0600

    j

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 from aiogram import Bot, Dispatcher
 from decouple import config
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
 TOKEN = config("TOKEN")
 bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
+dp = Dispatcher(bot=bot, storage=storage)
+try:
+    if a.count(int(message.text)) == 0:
+        async with state.proxy() as storage:
+            storage['id'] = message.from_user.id
+            storage['username'] = f"@{message.from_user.username}"
+            storage['mentor_id'] = int(message.text)
+            a.append(int(message.text))
+            await FSMAdmin.next()
+            await message.answer('Kak zvat?')
+except:
+    await message.answer('id zanyat poprobui drugoi id!!!')
--- main.py
+++ main.py
@@ -1,105 +1,13 @@
-from aiogram import types
+from aiogram import types, Dispatcher
 from aiogram.utils import executor
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import bot, dp
-import random
+from handlers import client, callback, extra, fsm_anketa
 
-@dp.message_handler(commands=['quiz'])
-async def quiz_1(message: types.Message):
-    markup = InlineKeyboardMarkup()
-    button_call_1 = InlineKeyboardButton("NEXT", callback_data='button_call_1')
-    markup.add(button_call_1)
-    question = "Кто является лучшим футболистом?"
-    answers = [
-        'Месси',
-        'Роналду',
-        'Роналдиньо',
-        'Марадонна',
-        'Пеле'
-    ]
-
-    await bot.send_poll(
-        chat_id=message.from_user.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=0,
-        explanation="IZI",
-        open_period=10,
-        reply_markup=markup
-    )
-
-@dp.callback_query_handler(lambda call: call.data == "button_call_1")
-async def quiz_2(call: types.CallbackQuery):
-    markup = InlineKeyboardMarkup
-    button_call_1 = InlineKeyboardButton("NEXT", callback_data='button_call_2')
-    markup.add(button_call_1)
-    question = "Сколько золотых мячей выиграл Месси?"
-    answers = [
-        "7",
-        "5",
-        "3",
-        "1",
-        "0"
-    ]
-
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=0,
-        explanation="IZI",
-        open_period=10,
-        reply_markup=markup
-    )
-
-
-@dp.callback_query_handler(lambda call: call.data == "button_call_2")
-async def quiz_3(call: types.CallbackQuery):
-    question = "Сколько букв в русском алфавите?"
-    answers = [
-        "36",
-        "33",
-        "27",
-        "29",
-        "32"
-    ]
-
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answers,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=1,
-        explanation="IZI",
-        open_period=10,
-    )
-
-@dp.message_handler(commands=['mem'])
-async def sendphoto(msg):
-        a = ['mems/mem1/mem1.jpg', 'mems/mem2/mem2.jpg', 'mems/mem3/mem3.jpg', 'mems/mem4/mem4.jpg', 'mems/mem5/mem5.jpg']
-        photo = open(random.choice(a), 'rb')
-        await bot.send_photo(chat_id=msg.from_user.id, photo=photo)
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    if message.text.isdigit() == True:
-        b = int(message.text)
-        await bot.send_message(message.chat.id, b*b)
-    elif message.reply_to_message == "!pin":
-        await bot.pin_chat_message(message.chat.id, message.message_id)
-    elif message.text.startswith("game"):
-        c = ['⚽', '🏀', '🎯', '🎰']
-        b = await bot.send_dice(message.chat.id, emoji=random.choice(c))
-    else:
-        await bot.send_message(message.from_user.id, message.text)
-
-
+fsm_anketa.register_handlers_fsm_anketa(dp)
+client.register_handlers_client(dp)
+callback.register_handlers_callback(dp)
+extra.registr_handlers_extra(dp)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
+
